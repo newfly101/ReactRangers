fed03e3522a42353adc5d302780132674361f263
brandysh <wh63000@gmail.com>
2024-06-08 23:37:17 +0900

    css를 모듈로 변경 및 추가

--- src/component/feed/Feed.js
+++ src/component/feed/Feed.js
@@ -2,7 +2,16 @@ import React from "react";
 import classes from "./Feed.module.css";
 
 const Feed = () => {
-  return <div></div>;
+  return (
+    <div className={classes.main}>
+      <div className={classes.mainFeed}>
+        <div className={classes.innerFeed}>
+          <h2 className={classes.nameFeed}>Feed</h2>
+          <p className={classes.pFont}>내가 구독하는 글입니다</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
