cf6ce0f18198a677cb467070383d4ec4a8e56ef6
Jae-Hong-Kim <newfly101@naver.com>
2024-06-08 13:43:26 +0900

    Router 추가 : nav와 footer 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import TistoryMain from "./component/TistoryMain";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Navigation from "./component/Navigation";
 
 function App() {
   return (
       <Router>
+          <div>
+              <Navigation />
+          </div>
           <Routes>
               <Route path="/" element={<TistoryMain />} />
+          {/*    
+            <Route path="/" element={<TistoryMain />} /> // 여기에 page link 추가
+            
+          */}
           </Routes>
+          <footer>
+              <div>
+                  <h2>Footer 입니다.</h2>
+              </div>
+          </footer>
       </Router>
   );
 }
---
+++ src/component/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav>
+            <div>
+                <a href="">Tistory</a>
+            </div>
+        </nav>
+
+    );
+};
+
+export default Navigation;
