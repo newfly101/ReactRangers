da34ec70c1ed3e54bec3fa38b2c09e44a87b72f7
Jae-Hong-Kim <newfly101@naver.com>
2024-06-08 16:00:54 +0900

    Navigation css 변경

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import classes from './Navigation.module.css';
+import {useLocation} from "react-router-dom";
 
 const Navigation = () => {
-    const navUrl = window.location.href.split('/');
-    console.log(navUrl[3]);
+    const location = useLocation();
+    console.log(location.pathname === '/forum/');
+
     return (
         <nav className={classes.navBar}>
-                <div className={classes.navBarBox}>
-                    <div className={classes.pageTitle}>
-                        <a href="/">Tistory</a>
-                    </div>
-                    <div className={classes.pageLinkBox}>
-                        <a href="/">피드</a>
-                    </div>
-                    <div className={classes.pageLinkBox}>
-                        <a href="/">스토리</a>
-                    </div>
-                    <div className={classes.pageLinkBox}>
-                        <a href="/">스킨</a>
-                    </div>
-                    <div className={classes.pageLinkBox}>
-                        <a href="/forum">포럼</a>
-                    </div>
+            <div className={classes.navBarBox}>
+                <div className={classes.pageTitle}>
+                    <a href="/">Tistory</a>
                 </div>
-                <div className={`${navUrl[3] !== 'forum' ?
-                    classes.navBarLogin : classes.navBarLogin2}`}>
-                    <button>시작하기</button>
+                <div className={classes.pageLinkBox}>
+                    <a href="/">피드</a>
                 </div>
+                <div className={classes.pageLinkBox}>
+                    <a href="/">스토리</a>
+                </div>
+                <div className={classes.pageLinkBox}>
+                    <a href="/">스킨</a>
+                </div>
+                <div className={location.pathname !== '/forum/' ? classes.pageLinkBox : classes.pageLinkedBox}>
+                    <a href="/forum">포럼</a>
+                </div>
+            </div>
+            <div className={`${location.pathname !== '/forum' ?
+                classes.navBarLogin : classes.navBarLogin2}`}>
+                <button>시작하기</button>
+            </div>
         </nav>
     );
 };
