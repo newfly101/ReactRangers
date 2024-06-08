71915081ba0abe6c7a5a9c97b7efa4b7a264255c
Jae-Hong-Kim <newfly101@naver.com>
2024-06-08 13:37:26 +0900

    main page 추가 및 Router 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import TistoryMain from "./component/TistoryMain";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <Router>
+          <Routes>
+              <Route path="/" element={<TistoryMain />} />
+          </Routes>
+      </Router>
   );
 }
 
---
+++ src/component/TistoryMain.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TistoryMain = () => {
+    return (
+        <div>
+            <h2>MainPage</h2>
+        </div>
+    );
+};
+
+export default TistoryMain;
