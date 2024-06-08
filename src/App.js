1323abb038c956782db91e20d4142060679deec6
brandysh <wh63000@gmail.com>
2024-06-08 23:18:36 +0900

    주석제거

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
 import TistoryMain from "./component/TistoryMain";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navigation from "./component/Navigation";
 import ForumMain from "./component/forum/ForumMain";
 
 function App() {
   return (
-      <Router>
-          <div>
-              <Navigation />
-          </div>
-          <Routes>
-              <Route path="/" element={<TistoryMain />} />
-              <Route path="/forum" element={<ForumMain />} />
-          {/*    
-            <Route path="/" element={<TistoryMain />} /> // 여기에 page link 추가
-            
-          */}
-          </Routes>
-          <footer>
-              <div>
-                  <h2>Footer 입니다.</h2>
-              </div>
-          </footer>
-      </Router>
+    <Router>
+      <div>
+        <Navigation />
+      </div>
+      <Routes>
+        <Route path="/" element={<TistoryMain />} />
+        <Route path="/forum" element={<ForumMain />} />
+      </Routes>
+      <footer>
+        <div>
+          <h2>Footer 입니다.</h2>
+        </div>
+      </footer>
+    </Router>
   );
 }
 
