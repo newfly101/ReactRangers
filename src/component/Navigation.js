44712c6b18e8831b18e0a90aa6e6294bccd9c73d
Jae-Hong-Kim <newfly101@naver.com>
2024-06-08 14:02:51 +0900

    nav bar 1차 css 수정

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import classes from './Navigation.module.css';
 
 const Navigation = () => {
     return (
-        <nav>
-            <div>
-                <a href="">Tistory</a>
-            </div>
+        <nav className={classes.navBar}>
+                <div className={classes.navBarBox}>
+                    <div className={classes.pageLinkBox}>
+                        <a href="">Tistory</a>
+                    </div>
+                    <div className={classes.pageLinkBox}>
+                        피드
+                    </div>
+                    <div className={classes.pageLinkBox}>
+                        스토리
+                    </div>
+                    <div className={classes.pageLinkBox}>
+                        스킨
+                    </div>
+                    <div className={classes.pageLinkBox}>
+                        포럼
+                    </div>
+                </div>
+                <div className={classes.navBarLogin}>
+                    <button>시작하기</button>
+                </div>
         </nav>
 
     );
