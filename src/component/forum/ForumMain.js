5a0c6420ccb2e833340f88f2df4ef7dd349c5f0e
Jae-Hong-Kim <newfly101@naver.com>
2024-06-08 15:44:22 +0900

    [J-Forum] 하드코딩 img 넣어줌

--- src/component/forum/ForumMain.js
+++ src/component/forum/ForumMain.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import classes from './ForumMain.module.css';
 
 const ForumMain = () => {
+    let RN = Number(Math.floor(Math.random() * 3)+1);
+    let img2 = `https://t1.daumcdn.net/tistory_admin/static/top/pc/bg_forum${RN}.jpg`;
     return (
         <div>
-            <h2>ForumMain Page</h2>
+            <div className={classes.forumHeader}>
+                <img src={img2} alt="fourmHeaderImg"/>
+            </div>
+            <div>
+                <input type="text"/>
+            </div>
         </div>
     );
 };
 
 export default ForumMain;
+
+
