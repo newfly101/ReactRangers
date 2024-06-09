6ed376b0b294eca1f7b9f7c2266ece66cbd46945
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-06-09 22:41:47 +0900

    구성요소 기본 배치 완료

--- src/component/story/StoryItemBox.js
+++ src/component/story/StoryItemBox.js
@@ -6,23 +6,28 @@ const storyItemBox = () => {
     <div>
       {dummyCategory1.data.list.map((item, index) => (
         <div key={index}>
-          <section className={classes.storyWrapper}>
+          <section className={classes.itemBoxWrapper}>
             <div className={classes.bottomItemBox}>
               <div className={classes.ItemBoxStart}>
-                <div>{dummyCategory1.data.list[index].categoryName}</div>
+                <div className={classes.categoryName}>
+                  {dummyCategory1.data.list[index].categoryName}
+                </div>
                 <div>{dummyCategory1.data.list[index].publishedAgo}</div>
                 <div>
                   공감{" "}
-                  <label>{dummyCategory1.data.list[index].likeCount}</label>
+                  <label className={classes.likeCount}>
+                    {dummyCategory1.data.list[index].likeCount}
+                  </label>
                 </div>
               </div>
 
               <div className={classes.ItemBoxMid}>
                 <div className={classes.ItemBoxTitle}>
-                  {dummyCategory1.data.list[index].title}
+                  {dummyCategory1.data.list[index].title.substr(0, 35)}
                 </div>
                 <div className={classes.ItemBoxdesc}>
-                  {dummyCategory1.data.list[index].summary}
+                  {dummyCategory1.data.list[index].summary.substr(0, 100) +
+                    "..."}
                 </div>
                 <div className={classes.ItemBoxAuthor}>
                   <img
@@ -31,11 +36,14 @@ const storyItemBox = () => {
                     alt="아이템박스 저자 이미지"
                   />
                   <div>{dummyCategory1.data.list[index].userName}</div>
-                  <div>by {dummyCategory1.data.list[index].blogTitle}</div>
+
+                  <div className={classes.blogName}>
+                    by {dummyCategory1.data.list[index].blogTitle}
+                  </div>
                 </div>
               </div>
 
-              <div>
+              <div className={classes.ItemBoxEnd}>
                 <img
                   className={classes.itemBoxImg}
                   src={`${dummyCategory1.data.list[index].thumbnail}`}
@@ -44,6 +52,7 @@ const storyItemBox = () => {
               </div>
             </div>
           </section>
+          <hr />
         </div>
       ))}
     </div>
