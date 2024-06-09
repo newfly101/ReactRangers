6c1dea2af388c73b7b9a5b2f465787b8b8b94341
Jae-Hong-Kim <newfly101@naver.com>
2024-06-09 22:55:52 +0900

    [J-Forum] 댓글부분 view css조정

--- src/component/forum/ForumBlog.js
+++ src/component/forum/ForumBlog.js
@@ -107,32 +107,37 @@ const ForumBlog = () => {
                         </div>
                     </div>
                     {activeIndex === index && showDetail === true &&
-                    <div className={classes.forumBlogListDetail} key={`'블로그Detail-'${index}`}>
-                        <div className={classes.forumBlogListComment}>
-                            <div className={classes.forumBlogListCommentImg}>
-                                <img src="https://tistory1.daumcdn.net/tistory/5581697/attach/bd3621800f28456f8807b54feb8e1ba7" alt="comImg" />
-                            </div>
-                            <div className={classes.forumBlogListCommentContext}>
-                                <div className={classes.forumContext}>
-                                    <div className={classes.forumContextHeader}>
-                                        숏힐링(Short Healing)
+                        blogTipDetailData.data.comments ? blogTipDetailData.data.comments.map((detailItem) =>
+                        (
+                            <div className={classes.forumBlogListDetail} key={`'블로그Detail-'${detailItem.id}`}>
+                                <div className={classes.forumBlogListComment}>
+                                    <div className={classes.forumBlogListCommentImg}>
+                                        <img
+                                            src={detailItem.userImage}
+                                            alt="comImg"/>
                                     </div>
-                                    ㆍ
-                                    <div className={classes.forumContextHeader}>
-                                        2024.06.08 17:16
-                                    </div>
-                                    ㆍ
-                                    <div className={classes.forumContextHeader}>
-                                        신고
+                                    <div className={classes.forumBlogListCommentContext}>
+                                        <div className={classes.forumContext}>
+                                            <div className={classes.forumContextHeader}>
+                                                {detailItem.userName}
+                                            </div>
+                                            ㆍ
+                                            <div className={classes.forumContextHeader}>
+                                                {detailItem.registered}
+                                            </div>
+                                            ㆍ
+                                            <div className={classes.forumContextHeader}>
+                                                신고
+                                            </div>
+                                        </div>
+                                        <div className={classes.forumContext}>
+                                            {detailItem.content}
+                                        </div>
                                     </div>
                                 </div>
-                                <div className={classes.forumContext}>
-                                    안녕하세요! 맞구독 신청해요~
-                                </div>
                             </div>
-                        </div>
-                    </div>
-                    }
+                        )
+                    ) : ''}
                 </div>
             ))}
         </div>
