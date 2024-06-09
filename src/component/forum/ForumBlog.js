a78a9ba6154b096f859a91e01745ea2ce08bd47f
Jae-Hong-Kim <newfly101@naver.com>
2024-06-09 16:02:01 +0900

    [J-Forum] 포럼view부분 Tap 추가

--- src/component/forum/ForumBlog.js
+++ src/component/forum/ForumBlog.js
@@ -6,14 +6,14 @@ const ForumBlog = () => {
     return (
         <div className={classes.forumBlogBox}>
             <div className={classes.forumBlogTitle}>
-                <div>
+                <div className={classes.forumBlogTap}>
                     <button>전체</button>
                     <button>블로그 소개</button>
                     <button>블로그 운영팁</button>
                     <button>스킨</button>
                     <button>질문/기타</button>
                 </div>
-                <div>
+                <div className={classes.forumWriteBtn}>
                     <button>글쓰기</button>
                 </div>
             </div>
