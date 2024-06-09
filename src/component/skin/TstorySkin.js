6b13a9e5e66d61fea4270e97439ce6b92b13bfd4
chochohee <168400614+chochohee@users.noreply.github.com>
2024-06-10 04:09:57 +0900

    위치조정 및 card 추가

--- src/component/skin/TstorySkin.js
+++ src/component/skin/TstorySkin.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from "./TstorySkin.module.css";
 import mainImage from "./SkinPageImage.jpg";
+import { ReactComponent as LinkIcon } from "./link.svg";
 
 const TstorySkin = () => {
   return (
@@ -14,14 +15,14 @@ const TstorySkin = () => {
           </h2>
         </div>
       </div>
-      <div className={classes.buttonContainer}>
-        <div className={classes.ButtonwrapUp}>
-          <div className={`${classes.allbutton} ${classes.buttontitle}`}>
+      <div className={`${classes.allContainer} ${classes.buttonContainer}`}>
+        <div className={classes.ButtonWrapUp}>
+          <div className={`${classes.allButton} ${classes.buttonTitle}`}>
             <button>티스토리 스킨</button>
             <button>이용자 제작 스킨</button>
           </div>
           <div
-            className={`${classes.allbutton} ${classes.hoverline} ${classes.tagButtonContainer}`}
+            className={`${classes.allButton} ${classes.hoverLine} ${classes.tagButtonContainer}`}
           >
             <button>#매거진</button>
             <button>#미니멀</button>
@@ -33,18 +34,33 @@ const TstorySkin = () => {
           </div>
         </div>
       </div>
-      <div className={classes.card_white}>
-        <div className={classes.allbutton}>
-          <button>
-            <h1>Odyssey</h1>
-          </button>
-        </div>
-        <p>글과 사진, 그리고 영상을 담아</p>
-        <p>당신의 블로그 여행에 함께합니다.</p>
-        <div className={`${classes.allbutton} ${classes.hoverline}`}>
-          <button>#반응형</button>
-          <button>#블로그형</button>
-          <button>#커버 지원</button>
+      <div className={`${classes.allContainer} ${classes.card_white}`}>
+        <div className={classes.card_wrap}>
+          <div className={`${classes.allButton} ${classes.card_title}`}>
+            <button>
+              <h1>Odyssey</h1>
+            </button>
+          </div>
+          <div className={` ${classes.card_text}`}>
+            <p>글과 사진, 그리고 영상을 담아</p>
+            <p>당신의 블로그 여행에 함께합니다.</p>
+          </div>
+          <div className={classes.card_button}>
+            <div className={`${classes.allButton} ${classes.hoverLine}`}>
+              <button>#반응형</button>
+              <button>#블로그형</button>
+              <button>#커버 지원</button>
+            </div>
+            <div className={`${classes.roundButton}`}>
+              <button>자세히 보기</button>
+              <button>
+                적용 예시 보기
+                <span className={classes.button_link_Icon}>
+                  <LinkIcon className={classes.icon} />
+                </span>
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
