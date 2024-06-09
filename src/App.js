323125db2212c8806efb1d69fb1190365bfbb8bc
rose <113295030+ros-e2@users.noreply.github.com>
2024-06-09 15:08:14 +0900

    app.js commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './main/main';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Main />
     </div>
   );
 }
