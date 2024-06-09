41b41d3aaf261db9bee7f7fb5386035d59e92598
brandysh <wh63000@gmail.com>
2024-06-09 16:06:34 +0900

    Follwing 페이지 기본 구성 및 css

---
+++ src/component/feed/Following.js
@@ -0,0 +1,24 @@
+import classes from "./Follwing.module.css";
+
+import React from "react";
+
+const Following = () => {
+  return (
+    <div className={classes.Fmain}>
+      <div className={classes.fMainFeed}>
+        <div className={classes.fInnerFeed}>
+          <h2 className={classes.fNameFeed}>Following</h2>
+          <div className={classes.followingFont}>
+            0개의 블로그를 구독중입니다
+            <div className={classes.fBtn}>
+              <button>최신 구독순</button>
+              <button>블로그 이름순</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Following;
