7b381191da9a9f7ed124d49a08b00a99bf1f3795
brandysh <wh63000@gmail.com>
2024-06-09 17:14:08 +0900

    Followind.js body부분 추가

--- src/component/feed/Following.js
+++ src/component/feed/Following.js
@@ -4,21 +4,30 @@ import React from "react";
 
 const Following = () => {
   return (
-    <div className={classes.Fmain}>
-      <div className={classes.fMainFeed}>
-        <div className={classes.fInnerFeed}>
-          <h2 className={classes.fNameFeed}>Following</h2>
-          <div className={classes.followingFont}>
-            0개의 블로그를 구독중입니다
-            <div className={classes.fBtn}>
-              <button type="button" className="btnFollow">
-                최신 구독순
-              </button>
-              <button type="button" className="btnFollow">
-                블로그 이름순
-              </button>
+    <div>
+      <div className={classes.Fmain}>
+        <div className={classes.fMainFeed}>
+          <div className={classes.fInnerFeed}>
+            <h2 className={classes.fNameFeed}>Following</h2>
+            <div className={classes.followingFont}>
+              0개의 블로그를 구독중입니다
+              <div className={classes.fBtn}>
+                <button type="button" className="btnFollow">
+                  최신 구독순
+                </button>
+                <button type="button" className="btnFollow">
+                  블로그 이름순
+                </button>
+              </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className={classes.fBody}>
+        <div className={classes.fList}>
+          <p className={classes.fBlog}>
+            아직 구독하는 블로그가 없습니다. 피드에서 추천 블로그를 확인하세요
+          </p>
         </div>
       </div>
     </div>
