5c2240a255085af41b1bb738b68da95e13407419
chochohee <168400614+chochohee@users.noreply.github.com>
2024-06-09 21:48:46 +0900

    skin link 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import Navigation from './component/Navigation';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navigation from "./component/Navigation";
 // import TistoryMain from './component/TistoryMain';
-import ForumMain from './component/forum/ForumMain';
-import Feed from './component/feed/Feed';
-import Admin from './component/admin/Admin';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import Main from './main/main';
-import Story from './component/story/Story';
-import './App.css';
+import ForumMain from "./component/forum/ForumMain";
+import Feed from "./component/feed/Feed";
+import Admin from "./component/admin/Admin";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Main from "./main/main";
+import Story from "./component/story/Story";
+import "./App.css";
+import Skin from "./component/skin/SkinPage";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         <Route path="/feed" element={<Feed />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/story" element={<Story />} />
+        <Route path="/skin" element={<Skin />} />
         {/*
             <Route path="/" element={<TistoryMain />} /> // 여기에 page link 추가
           */}
