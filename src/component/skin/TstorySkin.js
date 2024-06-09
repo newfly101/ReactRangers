725db29316f17ba7d19b5df1f2bc9c41e5473d70
chochohee <168400614+chochohee@users.noreply.github.com>
2024-06-10 02:34:24 +0900

    img alt 수정

--- src/component/skin/TstorySkin.js
+++ src/component/skin/TstorySkin.js
@@ -6,7 +6,7 @@ const TstorySkin = () => {
   return (
     <>
       <div className={classes.imageContaner}>
-        <img src={mainImage} className={classes.mainImage} />
+        <img src={mainImage} className={classes.mainImage} alt="" />
         <div className={classes.mainImage_textBox}>
           <h1 className={classes.mainImage_textBox_title}>Skin</h1>
           <h2 className={classes.mainImage_textBox_text}>
@@ -16,7 +16,7 @@ const TstorySkin = () => {
       </div>
       <div className={classes.buttonContainer}>
         <div className={classes.ButtonwrapUp}>
-          <div className={classes.allbutton}>
+          <div className={`${classes.allbutton} ${classes.buttontitle}`}>
             <button>티스토리 스킨</button>
             <button>이용자 제작 스킨</button>
           </div>
@@ -41,7 +41,7 @@ const TstorySkin = () => {
         </div>
         <p>글과 사진, 그리고 영상을 담아</p>
         <p>당신의 블로그 여행에 함께합니다.</p>
-        <div className={classes.allbutton}>
+        <div className={`${classes.allbutton} ${classes.hoverline}`}>
           <button>#반응형</button>
           <button>#블로그형</button>
           <button>#커버 지원</button>
