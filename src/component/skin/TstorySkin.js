c177d34dddb34df08e79ffe06b3f50ba95129214
chochohee <168400614+chochohee@users.noreply.github.com>
2024-06-10 14:43:08 +0900

    css상세조정 및 img hover boxshdow추가

--- src/component/skin/TstorySkin.js
+++ src/component/skin/TstorySkin.js
@@ -38,7 +38,7 @@ const TstorySkin = () => {
       <div className={classes.card0}>
         <div className={`${classes.allContainer} ${classes.cardWhite}`}>
           <div className={classes.cardWrap}>
-            <div className={`${classes.allButton} ${classes.buttonTitle}`}>
+            <div className={`${classes.allButton} ${classes.cardTitle}`}>
               <button>
                 <h1>Odyssey</h1>
               </button>
@@ -48,7 +48,53 @@ const TstorySkin = () => {
               <p>당신의 블로그 여행에 함께합니다.</p>
             </div>
             <div className={classes.cardButton}>
-              <div className={`${classes.allButton} ${classes.hoverLine}`}>
+              <div
+                className={`${classes.allButton} ${classes.hoverLine} ${classes.cardTagButton} `}
+              >
+                <button>#반응형</button>
+                <button>#블로그형</button>
+                <button>#커버 지원</button>
+              </div>
+              <div className={`${classes.roundButton}`}>
+                <button>자세히 보기</button>
+                <button className={classes.buttonLinkIcon}>
+                  적용 예시 보기
+                  <span>
+                    <LinkIcon className={classes.icon} />
+                  </span>
+                </button>
+              </div>
+            </div>
+            <div className={classes.cardImageContaner}>
+              <div className={classes.cardImage}>
+                <img src={cardImage} alt="" />
+              </div>
+              <div className={classes.cardImage}>
+                <img src={cardImage} alt="" />
+              </div>
+              <div className={classes.cardImage}>
+                <img src={cardImage} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={classes.card0}>
+        <div className={`${classes.allContainer} ${classes.cardGray}`}>
+          <div className={classes.cardWrap}>
+            <div className={`${classes.allButton} ${classes.cardTitle}`}>
+              <button>
+                <h1>Odyssey</h1>
+              </button>
+            </div>
+            <div className={` ${classes.cardText}`}>
+              <p>글과 사진, 그리고 영상을 담아</p>
+              <p>당신의 블로그 여행에 함께합니다.</p>
+            </div>
+            <div className={classes.cardButton}>
+              <div
+                className={`${classes.allButton} ${classes.hoverLine} ${classes.cardTagButton} `}
+              >
                 <button>#반응형</button>
                 <button>#블로그형</button>
                 <button>#커버 지원</button>
