9d62d6f8cf18ddbf72be09ec6c683cb11f359c4c
rose <113295030+ros-e2@users.noreply.github.com>
2024-06-10 15:39:07 +0900

    css보완

--- src/component/TistoryMain.js
+++ src/component/TistoryMain.js
@@ -64,32 +64,42 @@ const TistoryMain = () => {
         <div className={styles.section5}>
           <h5 className={styles.customH5}>더 잘 쓰고 싶어요?</h5>
           <p>
-            당신의 티스토리에 다양한&nbsp;
-            <span className={styles.customSpan}>플러그인</span>을 <br />
-            사용할 수 있어요.
+            <a href="https://notice.tistory.com/1797">
+              당신의 티스토리에 다양한&nbsp;
+              <span className={styles.customSpan}>플러그인</span>을 <br />
+              사용할 수 있어요.
+            </a>
           </p>
           <p>
-            사이트 보안을 위해 더 안전한
-            <br />
-            <span className={styles.customSpan}>암호화 접속</span>을
-            사용해보세요.
+            <a href="https://notice.tistory.com/2444">
+              사이트 보안을 위해 더 안전한
+              <br />
+              <span className={styles.customSpan}>암호화 접속</span>을
+              사용해보세요.
+            </a>
           </p>
           <p>
-            콘텐츠를 고정된 메뉴로 유지하고 싶다면
-            <br />
-            <span className={styles.customSpan}>페이지</span>를 이용해보세요.
+            <a href="https://notice.tistory.com/2400">
+              콘텐츠를 고정된 메뉴로 유지하고 싶다면
+              <br />
+              <span className={styles.customSpan}>페이지</span>를 이용해보세요.
+            </a>
           </p>
           <p>
-            나만의 <span className={styles.customSpan}> 도메인</span>을
-            연결해보세요.
-            <br />
-            당신의 티스토리에 엣지가 생길거예요.
+            <a href="https://notice.tistory.com/1784">
+              나만의 <span className={styles.customSpan}> 도메인</span>을
+              연결해보세요.
+              <br />
+              당신의 티스토리에 엣지가 생길거예요.
+            </a>
           </p>
           <p>
-            <span className={styles.customSpan}> 로그인 보안</span>기능으로
-            당신의 정보와
-            <br />
-            콘텐츠를 안전하게 지키세요!
+            <a href="https://notice.tistory.com/2384">
+              <span className={styles.customSpan}> 로그인 보안</span>기능으로
+              당신의 정보와
+              <br />
+              콘텐츠를 안전하게 지키세요!
+            </a>
           </p>
         </div>
       </div>
