0e31ca355326506b776b99bf4518b302b97f8467
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-06-10 16:21:02 +0900

    tag tab css 반응구현(하드코딩)

--- src/component/story/Story.js
+++ src/component/story/Story.js
@@ -1,9 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Story.module.css";
 import StoryBlog from "./StoryBlog";
 import StoryItemBox from "./StoryItemBox";
 
 const Story = () => {
+  const [tagUp0, setTagUp0] = useState(false);
+  const [tagUp1, setTagUp1] = useState(false);
+  const [tagUp2, setTagUp2] = useState(false);
+  const [tagUp3, setTagUp3] = useState(false);
+  const [tagUp4, setTagUp4] = useState(false);
+  const [tagUp5, setTagUp5] = useState(false);
+
+  const [tagView0, setTagView0] = useState(true);
+  const [tagView1, setTagView1] = useState(false);
+  const [tagView2, setTagView2] = useState(false);
+  const [tagView3, setTagView3] = useState(false);
+  const [tagView4, setTagView4] = useState(false);
+  const [tagView5, setTagView5] = useState(false);
+
+  const onHoverHandler = (num) => {
+    if (num === 0) {
+      setTagUp0(true);
+    } else if (num === 1) {
+      setTagUp1(true);
+    } else if (num === 2) {
+      setTagUp2(true);
+    } else if (num === 3) {
+      setTagUp3(true);
+    } else if (num === 4) {
+      setTagUp4(true);
+    } else if (num === 5) {
+      setTagUp5(true);
+    }
+  };
+
+  const onHoverOutHandler = (num) => {
+    if (num === 0) {
+      setTagUp0(false);
+    } else if (num === 1) {
+      setTagUp1(false);
+    } else if (num === 2) {
+      setTagUp2(false);
+    } else if (num === 3) {
+      setTagUp3(false);
+    } else if (num === 4) {
+      setTagUp4(false);
+    } else if (num === 5) {
+      setTagUp5(false);
+    }
+  };
+
+  const onClickHandler = (num) => {
+    if (num === 0) {
+      setTagView0(true);
+      setTagView1(false);
+      setTagView2(false);
+      setTagView3(false);
+      setTagView4(false);
+      setTagView5(false);
+    } else if (num === 1) {
+      setTagView0(false);
+      setTagView1(true);
+      setTagView2(false);
+      setTagView3(false);
+      setTagView4(false);
+      setTagView5(false);
+    } else if (num === 2) {
+      setTagView0(false);
+      setTagView1(false);
+      setTagView2(true);
+      setTagView3(false);
+      setTagView4(false);
+      setTagView5(false);
+    } else if (num === 3) {
+      setTagView0(false);
+      setTagView1(false);
+      setTagView2(false);
+      setTagView3(true);
+      setTagView4(false);
+      setTagView5(false);
+    } else if (num === 4) {
+      setTagView0(false);
+      setTagView1(false);
+      setTagView2(false);
+      setTagView3(false);
+      setTagView4(true);
+      setTagView5(false);
+    } else if (num === 5) {
+      setTagView0(false);
+      setTagView1(false);
+      setTagView2(false);
+      setTagView3(false);
+      setTagView4(false);
+      setTagView5(true);
+    }
+  };
   return (
     <div className={classes.storyMain}>
       <section className={`${classes.storyTopSection} ${classes.storyWrapper}`}>
@@ -15,12 +106,61 @@ const Story = () => {
         </div>
 
         <div className={classes.topSectionWrapTag}>
-          <div>라이프</div>
-          <div>여행.맛집</div>
-          <div>문화.연예</div>
-          <div>IT</div>
-          <div>스포츠</div>
-          <div>시사</div>
+          <div
+            className={classes.tags}
+            onMouseOver={() => onHoverHandler(0)}
+            onMouseOut={() => onHoverOutHandler(0)}
+            onClick={() => onClickHandler(0)}
+          >
+            라이프
+            {tagUp0 && <div className={classes.tagHr0} />}
+            {tagView0 && <div className={classes.tagHrView0} />}
+          </div>
+          <div
+            className={classes.tags}
+            onMouseOver={() => onHoverHandler(1)}
+            onMouseOut={() => onHoverOutHandler(1)}
+            onClick={() => onClickHandler(1)}
+          >
+            여행.맛집 {tagUp1 && <div className={classes.tagHr1} />}
+            {tagView1 && <div className={classes.tagHrView1} />}
+          </div>
+          <div
+            className={classes.tags}
+            onMouseOver={() => onHoverHandler(2)}
+            onMouseOut={() => onHoverOutHandler(2)}
+            onClick={() => onClickHandler(2)}
+          >
+            문화.연예 {tagUp2 && <div className={classes.tagHr2} />}
+            {tagView2 && <div className={classes.tagHrView2} />}
+          </div>
+          <div
+            className={classes.tags}
+            onMouseOver={() => onHoverHandler(3)}
+            onMouseOut={() => onHoverOutHandler(3)}
+            onClick={() => onClickHandler(3)}
+          >
+            IT {tagUp3 && <div className={classes.tagHr3} />}
+            {tagView3 && <div className={classes.tagHrView3} />}
+          </div>
+          <div
+            className={classes.tags}
+            onMouseOver={() => onHoverHandler(4)}
+            onMouseOut={() => onHoverOutHandler(4)}
+            onClick={() => onClickHandler(4)}
+          >
+            스포츠 {tagUp4 && <div className={classes.tagHr4} />}
+            {tagView4 && <div className={classes.tagHrView4} />}
+          </div>
+          <div
+            className={classes.tags}
+            onMouseOver={() => onHoverHandler(5)}
+            onMouseOut={() => onHoverOutHandler(5)}
+            onClick={() => onClickHandler(5)}
+          >
+            시사 {tagUp5 && <div className={classes.tagHr5} />}
+            {tagView5 && <div className={classes.tagHrView5} />}
+          </div>
         </div>
       </section>
       <section className={classes.storyMidSection}>
