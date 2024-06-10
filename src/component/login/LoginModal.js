e6a9cebef3aa7b46b31aab28b7d38014c55e24f0
Jae-Hong-Kim <newfly101@naver.com>
2024-06-11 02:06:54 +0900

    [J-Login] LoginModal 창 css 수정 Tistory Title 클릭 시 메인 페이지로 이동되면서 모달 창 꺼짐

--- src/component/login/LoginModal.js
+++ src/component/login/LoginModal.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import classes  from './LoginModal.module.css';
+import {Link} from "react-router-dom";
 
 class LoginModal extends Component {
     state= {
@@ -18,16 +19,20 @@ class LoginModal extends Component {
                 {isOpen ? (
                     <div className={classes.loginModal}>
                         <div className={classes.tistoryLogin}>
-                            <div style={{fontSize: "40px", fontWeight: "bold"}}>Tistory</div>
-                            <div style={{fontSize: "14px"}}>당신의 이야기가 값진 수익이 됩니다.</div>
+                            <div className={classes.tistoryLoginTitle} onClick={close}>
+                                <Link to="/">
+                                    Tistory
+                                </Link>
+                            </div>
+                            <div className={classes.loginDescription}>당신의 이야기가 값진 수익이 됩니다.</div>
                             <img src="https://t1.daumcdn.net/tistory_admin/static/top/pc/img_login.png" alt="loginImg"/>
-                            <button className={classes.kakaoLoginBtn} >
+                            <button className={classes.kakaoLoginBtn}>
                                 카카오 계정으로 로그인
                             </button>
-                            <div style={{fontSize: "13px", marginTop: "24px"}} >
-                                내 티스토리 계정을 모르겠어요
+                            <div className={classes.tistoryLoginHelp}>
+                            내 티스토리 계정을 모르겠어요
                             </div>
-                            <div style={{fontSize: "13px", marginTop: "24px"}} >
+                            <div className={classes.tistoryLoginHelp}>
                                 티스토리 이메일 계정으로 가입했었나요?
                             </div>
                         </div>
