697610fb6b6577c6257d5360807fdc78bce05b00
Jae-Hong-Kim <newfly101@naver.com>
2024-06-11 01:53:55 +0900

    [J-Login] 시작하기 버튼 클릭 시 모달 띄우게 구현

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -1,16 +1,20 @@
 import React, {useEffect} from "react";
 import classes from "./Navigation.module.css";
 import {Link, useLocation} from "react-router-dom";
+import LoginModal from "./login/LoginModal";
 
 const Navigation = () => {
+    const [isOpen, setIsOpen] = React.useState(false);
     const [scrolled, setScrolled] = React.useState(false);
     const [url, setUrl] = React.useState("/");
     const location = useLocation();
 
-    const onClickAdminPage = () => {
-        console.log(location.pathname);
-        setUrl(location.pathname);
-    };
+    const openLoginModal = () => {
+        setIsOpen(true);
+    }
+    const closeLoginModal = () => {
+        setIsOpen(false);
+    }
 
     useEffect(() => {
         const handleScroll = () => {
@@ -33,7 +37,9 @@ const Navigation = () => {
     }, [location.pathname]);
 
     return (
-        <nav className={scrolled ? classes.navBar : classes.navBarBlank}>
+        <div>
+            {isOpen && <div className={classes.backdrop} onClick={closeLoginModal}></div>}
+            <nav className={scrolled ? classes.navBar : classes.navBarBlank}>
             {(url !== "/forum" && url !== "/skin") ?
                 <div className={classes.navBarBox}>
                     <div className={classes.pageTitle}>
@@ -75,11 +81,16 @@ const Navigation = () => {
             <div
                 className={classes.navBarLogin}
             >
-                <button onClick={onClickAdminPage}>
+                <button onClick={openLoginModal}>
                     <Link to="/admin">시작하기</Link>
                 </button>
             </div>
+            {isOpen && <LoginModal
+                isOpen={isOpen}
+                close={closeLoginModal}
+            />}
         </nav>
+        </div>
     );
 };
 
