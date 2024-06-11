04b3dba61657850dad4656a6115d3c3ac060d196
Jae-Hong-Kim <newfly101@naver.com>
2024-06-12 02:41:01 +0900

    [J-common] 배포 페이지 오류 수정 HOT FIX

--- src/component/accountSreach/AccountRecovery.js
+++ src/component/accountSreach/AccountRecovery.js
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from "./AccountRecovery.module.css";
+// import classes from "./AccountRecovery.module.css";
 
 const AccountRecovery = () => {
   return (
