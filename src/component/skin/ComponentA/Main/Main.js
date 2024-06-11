21bb34a2753c06864f14741412e6a0ccd8595d18
chochohee <168400614+chochohee@users.noreply.github.com>
2024-06-11 15:02:34 +0900

    main class/css 수정

--- src/component/skin/ComponentA/Main/Main.js
+++ src/component/skin/ComponentA/Main/Main.js
@@ -5,11 +5,11 @@ import mainImage from "../Main/Main.jpg";
 const Main = () => {
   return (
     <>
-      <div className={classes.imageContaner}>
+      <div className={classes.contaner}>
         <img src={mainImage} className={classes.mainImage} alt="" />
-        <div className={classes.mainImageTextBox}>
-          <h1 className={classes.mainImageTextBoxTitle}>Skin</h1>
-          <h2 className={classes.mainImageTextBoxText}>
+        <div className={classes.textBox}>
+          <h1 className={classes.title}>Skin</h1>
+          <h2 className={classes.text}>
             다양한 스킨으로 나만의 다른 공간을 만들어보세요.
           </h2>
         </div>
