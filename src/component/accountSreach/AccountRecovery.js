9167d126b1a14e3c74a7feaab62da0758f8deb81
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-06-12 04:07:04 +0900

    [accountRecovery]js기능 구현 완료

--- src/component/accountSreach/AccountRecovery.js
+++ src/component/accountSreach/AccountRecovery.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./AccountRecovery.module.css";
 import check from "./check.svg";
 const AccountRecovery = () => {
+  const [boldFlag, setBoldFlag] = useState(true);
+
+  const methodClickHandler = () => {
+    setBoldFlag((prevState) => !prevState);
+  };
   return (
     <div className={classes.AccountRecoveryWrapper}>
       <div className={classes.topText}>
@@ -20,19 +25,33 @@ const AccountRecovery = () => {
               className={classes.radio}
               readOnly
             />
-            <span>계정(이메일)찾기</span>
+            <span
+              onClick={methodClickHandler}
+              className={`${classes.emailMethod} ${boldFlag && classes.methodBold}`}
+            >
+              계정(이메일)찾기
+            </span>
             <img src={check} className={classes.check1} alt="체크표시" />
           </label>
           <label>
             <input type="radio" name="method" className={classes.radio} />
-            <span>비밀번호 찾기</span>
+            <span
+              onClick={methodClickHandler}
+              className={`${classes.emailMethod} ${!boldFlag && classes.methodBold}`}
+            >
+              비밀번호 찾기
+            </span>
             <img src={check} className={classes.check2} alt="체크표시" />
           </label>
         </form>
         <input
           type="text"
           className={classes.inputTextBox}
-          placeholder="가입된 이메일 주소를 입력해주세요."
+          placeholder={
+            boldFlag
+              ? "내 블로그 주소를 입력해주세요."
+              : "가입된 이메일 주소를 입력해주세요."
+          }
         ></input>
       </div>
       <div className={classes.commitBtn}>확인</div>
