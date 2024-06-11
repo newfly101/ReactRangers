3407b6aa2c7e73f0d92abf08718790cd8d27aade
Jae-Hong-Kim <newfly101@naver.com>
2024-06-12 00:27:41 +0900

    [J-Forum] 검색창 component 구성

--- src/component/forum/ForumMain.js
+++ src/component/forum/ForumMain.js
@@ -13,6 +13,20 @@ const ForumMain = () => {
                 <div className={classes.forumSearch}>
                     <div className={classes.forumTitle}>Forum</div>
                     <div className={classes.forumDescription}>티스토리에 대한 이야기를 자유롭게 나눠보세요</div>
+                    <div className={classes.forumSearchBox}>
+                        <div className={classes.forumSearchBoxLabel}>
+                            제목
+                        </div>
+                        <div className={classes.forumSearchBoxImg}>
+                            <img src={'/Forum/arrow-down.svg'} alt="arrowDown"/>
+                        </div>
+                        <div className={classes.forumSearchBoxInput}>
+                            <input placeholder="검색어를 입력하세요"/>
+                        </div>
+                        <div className={classes.forumSearchBoxImg}>
+                            <img src={'/Forum/search.svg'} alt="search"/>
+                        </div>
+                    </div>
                 </div>
             </div>
             <ForumBlog/>
