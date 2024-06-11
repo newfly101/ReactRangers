c0650eae9352e06234c8336af6691e443cff85c9
inagi94 <minami8520@nate.com>
2024-06-11 15:49:54 +0900

    Admin 텍스트, 왼쪽css 추가

--- src/component/admin/Admin.js
+++ src/component/admin/Admin.js
@@ -3,19 +3,21 @@ import classes from "./Admin.module.css";
 
 const Admin = () => {
   return (
-    <div className={classes.Admin}>
-      <div className={classes.AdminLeft}>
-        <div className={classes.AdminProfile}>
+    <div id={classes.Admin}>
+      <div id={classes.AdminLeft}>
+        <div id={classes.AdminProfile}>
           <div className={classes.AdminProfileImage}>
             <a href="">
-              <img src="" alt></img>
+              <img src="" width="100" height="100" alt></img>
             </a>
           </div>
           <div className={classes.AdminName}>
             <div className={classes.AdminNameUser}>새벽감성개발자</div>
             <div className={classes.AdminNameEmail}>game@naver.com</div>
           </div>
-          <div className={classes.AdminList}>
+        </div>
+        <div id={classes.AdminList}>
+          <div>
             <div>
               <div className={classes.AdminListHomeIcon}></div>
               <div className={classes.AdminListMyblog}>내 블로그</div>
@@ -34,7 +36,7 @@ const Admin = () => {
           </div>
         </div>
       </div>
-      <div className={classes.AdminRight}>
+      <div id={classes.AdminRight}>
         <div className={classes.AdminMyblog}>
           <h3 className={classes.AdminFont}>
             운영 중인 블로그
@@ -44,10 +46,17 @@ const Admin = () => {
                   <img src=""></img>
                 </a>
               </div>
-              <div className={classes.AdminBlockName}></div>
-              <div className={classes.AdminBlockEmail}></div>
-              <button className={classes.AdminBlockButton}>대표</button>
+              <div className={classes.AdminBlockName}>
+                새벽의 이것저것
+                <div className={classes.AdminBlockEmail}>game.tistory.com</div>
+              </div>
+              <div className={classes.AdminBlockButton}>
+                <button>대표</button>
+              </div>
             </form>
+            <div className={classes.Admin}>
+              대표 블로그는 방문한 블로그/팀블로그 활동시 사용됩니다.
+            </div>
           </h3>
           <h3 className={classes.AdminFont}>
             운영∙개설 현황
