d9a904882fb2a59c23b0ce3173512c42f83d551a
brandysh <wh63000@gmail.com>
2024-06-11 16:48:32 +0900

    check아이콘 추가

--- src/component/feed/Following.js
+++ src/component/feed/Following.js
@@ -14,9 +14,41 @@ const Following = () => {
               <div className={classes.fBtn}>
                 <button type="button" className={classes.btnFollow}>
                   최신 구독순
+                  <span className={classes.checkImg}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="size-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m4.5 12.75 6 6 9-13.5"
+                      />
+                    </svg>
+                  </span>
                 </button>
                 <button type="button" className={classes.btnFollow}>
                   블로그 이름순
+                  <span className={classes.checkImg}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="size-6"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="m4.5 12.75 6 6 9-13.5"
+                      />
+                    </svg>
+                  </span>
                 </button>
               </div>
             </div>
