0c0f7066a6f6e868d2cc4d4d5975246549613e33
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-06-11 17:04:33 +0900

    [Story] a태그로 각 블로그 연결

--- src/component/story/StoryBlog.js
+++ src/component/story/StoryBlog.js
@@ -54,43 +54,46 @@ const StoryBlog = ({ useTag }) => {
   return (
     <div className={classes.storyMidSection}>
       {carditems.data.list.map((item, index) => (
-        <div
-          className={classes.blogWrapper}
-          key={index}
-          onMouseOver={() => subBtnHandler(index)}
-          onMouseOut={() => subBtnHandlerOut(index)}
-        >
-          <img
-            src={`${item.blogImage}`}
-            alt="스토리블로그 이미지"
-            className={classes.blogImg}
-          />
+        <a href={item.blogUrl} key={index}>
+          <div
+            className={classes.blogWrapper}
+            onMouseOver={() => subBtnHandler(index)}
+            onMouseOut={() => subBtnHandlerOut(index)}
+          >
+            <img
+              src={`${item.blogImage}`}
+              alt="스토리블로그 이미지"
+              className={classes.blogImg}
+            />
 
-          <div className={classes.blogTextWrapper}>
-            <div className={classes.blogInfo}>
-              {item.serviceCategoryName} &nbsp;·&nbsp; 공감
-              <label className={classes.blogInfoNum}>
-                {" "}
-                {item.reactionCount}{" "}
-              </label>
-              &nbsp; ·&nbsp; 응원{" "}
-              <label className={classes.blogInfoNum}>{item.commentCount}</label>
-            </div>
-
-            <div className={classes.blogTitle}>{item.entryTitle}</div>
-            <div className={classes.blogFooter}>
-              <div>
-                <div className={classes.blogDesc}>
-                  {item.blogTitle.substr(0, 15)}
-                </div>
-                <div className={classes.blogAuthor}>by {item.authorName}</div>
+            <div className={classes.blogTextWrapper}>
+              <div className={classes.blogInfo}>
+                {item.serviceCategoryName} &nbsp;·&nbsp; 공감
+                <label className={classes.blogInfoNum}>
+                  {" "}
+                  {item.reactionCount}{" "}
+                </label>
+                &nbsp; ·&nbsp; 응원{" "}
+                <label className={classes.blogInfoNum}>
+                  {item.commentCount}
+                </label>
               </div>
-              {subFlag[index] && (
-                <div className={classes.subscribeBtn}>구독하기</div>
-              )}
+
+              <div className={classes.blogTitle}>{item.entryTitle}</div>
+              <div className={classes.blogFooter}>
+                <div>
+                  <div className={classes.blogDesc}>
+                    {item.blogTitle.substr(0, 15)}
+                  </div>
+                  <div className={classes.blogAuthor}>by {item.authorName}</div>
+                </div>
+                {subFlag[index] && (
+                  <div className={classes.subscribeBtn}>구독하기</div>
+                )}
+              </div>
             </div>
           </div>
-        </div>
+        </a>
       ))}
     </div>
   );
