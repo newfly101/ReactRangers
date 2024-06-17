72420cc4e2d57f9c1a1afb0fdb8b725e98b68d50
Jae-Hong-Kim <newfly101@naver.com>
2024-06-18 00:44:19 +0900

    [스킨-KJH] 카드 뷰 추가

--- src/component/skin/ComponentA/SkinPage/skinpageTest/SkinPageKJH.js
+++ src/component/skin/ComponentA/SkinPage/skinpageTest/SkinPageKJH.js
@@ -1,8 +1,10 @@
-import React from "react";
-// import classes from "../SkinPage.module.css";
+import React, {useEffect} from "react";
+import { ReactComponent as LinkIcon } from "../link.svg";
 import classes from "./SkinPageKJH.module.css";
 
 const SkinPageKJH = ({items}) => {
+    const [imageClick, setImageClick] = React.useState(false);
+    const [listindex, setListIndex] = React.useState(0);
 
     // {
     //   "type": "cardWhite",
@@ -18,12 +20,17 @@ const SkinPageKJH = ({items}) => {
     // },
     console.log(items);
     const labelData = items.filter((item) => item.type === "label");
-    console.log(labelData);
+    const cardsData = items.filter((item) => item.type !== "label");
+    console.log("cardsData",cardsData);
+
+    useEffect(() => {
+
+    }, [listindex]);
 
     return (
         <>
             {labelData.map((item, index) => {
-                console.log("item.data",item.data);
+                // console.log("item.data",item.data);
                 return (
                     <div key={`label${index}`} className={classes.labelContainer}>
                         <div className={classes.labelWrap}>
@@ -36,6 +43,69 @@ const SkinPageKJH = ({items}) => {
                             ))}
                             </div>
                         </div>
+                    </div>
+                )
+            })}
+            {cardsData.map((item, index) => {
+                // console.log("cardsData map:",item);
+                const content = item.data.content;
+                const img = item.data.img;
+                const tag = item.data.tag;
+                return (
+                    <div className={`${classes.container} ${classes[item.type]}`} key={index}>
+                        <div className={classes.CardTextBox}>
+                            <div className={classes.title}>
+                                {item.data.title}
+                            </div>
+                            <div className={classes.content}>
+                                {content.map((contentTxt, index) => (
+                                    <span key={`content${index}`}>{contentTxt}</span>
+                                ))}
+                            </div>
+                            <div className={classes.tagButtonBox}>
+                                <div className={classes.tagBox}>
+                                    {tag.map((tagTxt, index) => (
+                                        <div className={classes.cardTag} key={`content${index}`}>{tagTxt}</div>
+                                    ))}
+                                </div>
+                                <div className={classes.buttonBox}>
+                                    <button>자세히 보기</button>
+                                    <button className={classes.buttonLinkIcon}>
+                                        적용 예시 보기
+                                        <LinkIcon className={classes.icon}/>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={classes.imageContainer}>
+                            {img.map((imgSrc, index) => (
+                                <div key={`cardImg-${index}`}>
+                                    <img src={`/SkinImg/${item.data.title}/${imgSrc}`} alt={`cardImg-${index}`} />
+                                </div>
+                            ))}
+                                <div >
+                                    {/*{imageClick === false ? (*/}
+                                    {/*    <div className={classes.rightArrow}>*/}
+                                    {/*        <button onClick={clickRightArrow}>*/}
+                                    {/*            <img*/}
+                                    {/*                src={"/SkinImg/arrow-right.svg"}*/}
+                                    {/*                alt="arrowRight"*/}
+                                    {/*            />*/}
+                                    {/*        </button>*/}
+                                    {/*    </div>*/}
+                                    {/*) : (*/}
+                                    {/*    <div className={classes.leftArrow}>*/}
+                                    {/*        <button onClick={clickLeftArrow}>*/}
+                                    {/*            <img*/}
+                                    {/*                src={"/SkinImg/arrow-left.svg"}*/}
+                                    {/*                alt="arrowLeft"*/}
+                                    {/*            />*/}
+                                    {/*        </button>*/}
+                                    {/*    </div>*/}
+                                    {/*)}*/}
+                                </div>
+                            </div>
                     </div>
                 )
             })}
