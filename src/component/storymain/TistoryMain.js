4da4e2c4e6efde1584507e5aa741941dd6d07128
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-06-17 19:54:46 +0900

    [storymain]컴포넌트 분리 완료

--- src/component/storymain/TistoryMain.js
+++ src/component/storymain/TistoryMain.js
@@ -4,11 +4,12 @@ import "slick-carousel/slick/slick-theme.css";
 import classes from "./TistoryMain.module.css";
 import StoryMainHeader from "./StoryMainHeader";
 import StoryMainContent from "./StoryMainContent";
+import StoryMainBanner from "./StoryMainBanner";
 
 const TistoryMain = () => {
   return (
-    <div className="App">
-      <div className="header">
+    <div className={classes.App}>
+      <div>
         <StoryMainHeader />
         <div>
           <img
@@ -20,47 +21,7 @@ const TistoryMain = () => {
       </div>
       <div>
         <StoryMainContent />
-        <div className={classes.section5}>
-          <h5 className={classes.customH5}>더 잘 쓰고 싶어요?</h5>
-          <p>
-            <a href="https://notice.tistory.com/1797">
-              당신의 티스토리에 다양한&nbsp;
-              <span className={classes.customSpan}>플러그인</span>을 <br />
-              사용할 수 있어요.
-            </a>
-          </p>
-          <p>
-            <a href="https://notice.tistory.com/2444">
-              사이트 보안을 위해 더 안전한
-              <br />
-              <span className={classes.customSpan}>암호화 접속</span>을
-              사용해보세요.
-            </a>
-          </p>
-          <p>
-            <a href="https://notice.tistory.com/2400">
-              콘텐츠를 고정된 메뉴로 유지하고 싶다면
-              <br />
-              <span className={classes.customSpan}>페이지</span>를 이용해보세요.
-            </a>
-          </p>
-          <p>
-            <a href="https://notice.tistory.com/1784">
-              나만의 <span className={classes.customSpan}> 도메인</span>을
-              연결해보세요.
-              <br />
-              당신의 티스토리에 엣지가 생길거예요.
-            </a>
-          </p>
-          <p className={classes.customParagraph}>
-            <a href="https://notice.tistory.com/2384">
-              <span className={classes.customSpan}> 로그인 보안</span>기능으로
-              당신의 정보와
-              <br />
-              콘텐츠를 안전하게 지키세요!
-            </a>
-          </p>
-        </div>
+        <StoryMainBanner />
       </div>
     </div>
   );
