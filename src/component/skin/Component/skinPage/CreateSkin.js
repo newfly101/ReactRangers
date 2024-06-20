c75b333aa9d49161375e58828e64d535f5d7a499
chochohee <168400614+chochohee@users.noreply.github.com>
2024-06-20 21:21:38 +0900

    [skin] Component 폴더명 변경 및 스킨등록추가

---
+++ src/component/skin/Component/skinPage/CreateSkin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CreateSkin = () => {
+    return (
+        <>
+        <div>
+            스킨 제목
+        </div>
+        </>
+    );
+};
+
+export default CreateSkin;
