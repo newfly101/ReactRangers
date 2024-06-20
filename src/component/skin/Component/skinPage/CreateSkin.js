08884a03740b963cce9eaf57dd9b1f63838006f7
chochohee <168400614+chochohee@users.noreply.github.com>
2024-06-20 22:49:47 +0900

    [skin] createskin css생성

--- src/component/skin/Component/skinPage/CreateSkin.js
+++ src/component/skin/Component/skinPage/CreateSkin.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import classes from "./CreateSkin.module.css";
 
 const CreateSkin = () => {
     return (
         <>
-            <div>
+            <div className={classes.container}>
+                <div className={classes.cardWrap}>
                 <form>
-                    <fieldset>
-                        <div></div>
+                    <fieldset className={classes.fieldset}>
+                        <div className={classes.title}>
+                            <div><label>스킨 제목</label></div>
+                            <div>
+                                <input type="text" placeholder={"스킨명을 입력하세요"}/>
+                            </div>
+                        </div>
                     </fieldset>
                 </form>
+                </div>
             </div>
         </>
     );
