d550d9fde17e1c4010dfa82735bb2e75d3842971
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-06-21 04:00:04 +0900

    [피드 페이지] mobx 적용 및 js구현 완료

--- src/component/feed/Feed.js
+++ src/component/feed/Feed.js
@@ -3,18 +3,28 @@ import FeedTopText from "./FeedTopText";
 import FeedMidBox from "./FeedMidBox";
 import FeedBottomCard from "./FeedBottomCard";
 import classes from "./Feed.module.css";
+import FeedStore from "../../stores/FeedStore";
+import {Observer} from "mobx-react";
 
 const Feed = () => {
-
+const feedStore = new FeedStore();
   return (
-    <>
-      <div className={classes.Wrapper}>
-        <FeedTopText/>
-        <FeedMidBox/>
-        <FeedBottomCard/>
-      </div>
-
-    </>
+    <Observer>
+      {()=>(
+        <>
+          <div className={classes.Wrapper}>
+            <FeedTopText feedStore={feedStore}/>
+            {feedStore.feedPage === 'Follower' || feedStore.feedPage === 'Following'
+              ? <div className={classes.followText}>{feedStore.followMidText}</div> :
+              <>
+                <FeedMidBox/>
+                <FeedBottomCard/>
+              </>
+            }
+          </div>
+        </>
+      )}
+    </Observer>
   );
 };
 
--- src/component/feed/FeedTopText.js
+++ src/component/feed/FeedTopText.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import classes from "./FeedTopText.module.css";
+import {Observer} from "mobx-react";
 
-const FeedTopText = () => {
-  return (
-    <div className={classes.topTextContainer}>
-      <div className={classes.topTitle}>Feed</div>
-      <div className={classes.subscribeInfo}>
-        <div className={classes.topText}>내가 구독하는 글입니다.</div>
-        <div className={classes.subscribeWrapper}>
-          <span className={classes.subscribingNum}> 구독중
-            <span className={classes.subscribeNum}>0</span>
+const FeedTopText = ({feedStore}) => {
+
+  return (<Observer>
+      {() => (<div className={classes.topTextContainer}>
+          <div className={classes.topTitle}>{feedStore.feedPage}</div>
+          <div className={classes.subscribeInfo}>
+            <div className={classes.topText}>{feedStore.feedPageText}</div>
+            <div className={classes.subscribeWrapper}>
+              {feedStore.feedPage === 'Follower' || feedStore.feedPage === 'Following' ?
+                <><span className={classes.subscribingNum}>최신 구독순</span><span className={classes.subscribingNum}>블로그 이름순</span></> :
+                <><span className={classes.subscribingNum} onClick={feedStore.clickSubscribeing}> 구독중 <span className={classes.subscribeNum}>0</span>
           </span>
-          <span className={classes.subscribingNum}> 구독자
-            <span className={classes.subscribeNum}>0</span>
-          </span>
-        </div>
-      </div>
-    </div>
-  );
+                <span className={classes.subscribingNum} onClick={feedStore.clickSubscribed}> 구독자 <span className={classes.subscribeNum}>0</span>
+                </span></>
+              }
+
+            </div>
+          </div>
+        </div>)}
+    </Observer>);
 };
 
 export default FeedTopText;
