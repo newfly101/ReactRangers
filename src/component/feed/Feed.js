fa49ad633eeaa9778120f95153c14204ca34e45e
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-06-21 02:15:56 +0900

    [피드 페이지] 뷰 완성

--- src/component/feed/Feed.js
+++ src/component/feed/Feed.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import FeedTopText from "./FeedTopText";
+import FeedMidBox from "./FeedMidBox";
+import FeedBottomCard from "./FeedBottomCard";
+import classes from "./Feed.module.css";
 
 const Feed = () => {
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className={classes.Wrapper}>
+        <FeedTopText/>
+        <FeedMidBox/>
+        <FeedBottomCard/>
+      </div>
+
+    </>
   );
 };
 
