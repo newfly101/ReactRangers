cf9ea3130b1d637918cf7c6a1fcdac3404834e99
Jae-Hong-Kim <newfly101@naver.com>
2024-06-23 02:09:44 +0900

    [mobx:AuthStore] 로그인 form생성

--- src/component/convertAccount/ConvertForm.js
+++ src/component/convertAccount/ConvertForm.js
@@ -2,25 +2,50 @@ import React, {useState} from "react";
 import classes from "./ConvertForm.module.css";
 import {Link} from "react-router-dom";
 import {PathUrl} from "../../stores/CommonStore";
+import {Observer, useLocalObservable} from "mobx-react";
+import AuthStore from "../../stores/AuthStore";
 
 const ConvertForm = () => {
+    const authStore = useLocalObservable(AuthStore);
+
+    const handleOnLoginSubmit = (event) => {
+        event.preventDefault();
+        console.log(authStore.login.id);
+        console.log(authStore.login.email);
+        console.log(authStore.login.password);
+    }
+    const onChangeLoginEmail = (email) => {
+        authStore.login.email = email;
+    }
+    const onChangeLoginPassword = (password) => {
+        authStore.login.password = password;
+    }
 
     return (
+        <Observer>
+            {() => (
             <div className={classes.loginBox}>
-                <div>티스토리에 로그인 하세요.</div>
-                <form>
+                <h2>티스토리에 로그인 하세요.</h2>
+                <form onSubmit={handleOnLoginSubmit}>
                     <div><input
                         className={classes.emailInput}
                         type="email"
                         placeholder="이메일"
+                        value={authStore.login.email}
+                        onChange={(event) => onChangeLoginEmail(event.target.value)}
                     /></div>
                     <div>
                         <input
                             className={classes.passwordInput}
                             type="password"
                             placeholder="비밀번호"
+                            value={authStore.login.password}
+                            onChange={(event) => onChangeLoginPassword(event.target.value)}
                         />
                     </div>
+                    <button type="submit">
+                        로그인
+                    </button>
                 </form>
                 <div className={classes.linkBox}>
                     <span className={classes.loginLink}>
@@ -32,6 +57,8 @@ const ConvertForm = () => {
                     </span>
                 </div>
             </div>
+            )}
+        </Observer>
     );
 };
 
--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -13,6 +13,7 @@ export const ModalState = {
 
 const EmptyLogin = {
     id: '',
+    email: '',
     password: ''
 }
 
