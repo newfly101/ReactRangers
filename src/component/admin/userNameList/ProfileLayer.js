bc15275281beacbb6233f2122b084d46bcfb3762
aa1as <sm13929881@gmail.com>
2024-06-23 23:16:16 +0900

    profileLayer 코드변경 및 카멜케이스 적용

--- src/component/admin/userNameList/ProfileLayer.js
+++ src/component/admin/userNameList/ProfileLayer.js
@@ -4,30 +4,34 @@ import MyProfile from "../MyProfile";
 import ProfileLayerFilter from "./ProfileLayerFilter";
 
 const ProfileLayer = () => {
-  const [filteredReceive, setFilteredReceive] = useState();
-
-  const changeFilterHandler = (selectedValue) => {
-    setFilteredReceive(
-      selectedValue === "공개합니다." ? "공개합니다." : "공개하지 않습니다."
-    );
-  };
-
+  const [filteredReceive, setFilteredReceive] = useState("공개하지 않습니다.");
   const [sites, setSites] = useState([
     { name: "새벽감성개발자", url: "game@naver.com" },
   ]);
   const [newSite, setNewSite] = useState({ name: "tistory", url: "http://" });
 
-  const handleAddSite = () => {
-    setSites([...sites, newSite]);
-    setNewSite({ name: "tistory", url: "http://" });
+  const changeFilterHandler = (selectedValue) => {
+    setFilteredReceive(selectedValue);
   };
 
-  const handleInputChange = (e, index) => {
-    const { name, value } = e.target;
+  const handleAddSite = () => {
+    if (newSite.name && newSite.url) {
+      setSites([...sites, newSite]);
+      setNewSite({ name: "tistory", url: "http://" });
+    }
+  };
+
+  const handleInputChange = (event, index) => {
+    const { name, value } = event.target;
     const updatedSites = sites.map((site, i) =>
       i === index ? { ...site, [name]: value } : site
     );
     setSites(updatedSites);
+  };
+
+  const handleNewSiteChange = (event) => {
+    const { name, value } = event.target;
+    setNewSite({ ...newSite, [name]: value });
   };
 
   const handleDeleteSite = (index) => {
@@ -37,14 +41,14 @@ const ProfileLayer = () => {
 
   return (
     <div id={classes.profileLayer}>
-      <div id={classes.AdminLeft}>
+      <div id={classes.adminLeft}>
         <MyProfile />
       </div>
-      <div id={classes.AdminRight}>
+      <div id={classes.adminRight}>
         <div>
-          <div className={classes.AdminFont}>이메일 알림</div>
+          <div className={classes.mainTitle}>프로필 레이어</div>
           <div>
-            <div className={classes.AdminBlock1}>
+            <div className={classes.adminBlock1}>
               <ProfileLayerFilter
                 selected={filteredReceive}
                 onChangeFilter={changeFilterHandler}
@@ -52,7 +56,7 @@ const ProfileLayer = () => {
             </div>
           </div>
         </div>
-        <div className={classes.AdminBlock2}>
+        <div className={classes.adminBlock2}>
           <label>표시하는 사이트</label>
           <div>
             {sites.map((site, index) => (
@@ -66,14 +70,14 @@ const ProfileLayer = () => {
                   type="text"
                   name="name"
                   value={site.name}
-                  onChange={(e) => handleInputChange(e, index)}
+                  onChange={(event) => handleInputChange(event, index)}
                   className={classes.input}
                 />
                 <input
                   type="text"
                   name="url"
                   value={site.url}
-                  onChange={(e) => handleInputChange(e, index)}
+                  onChange={(event) => handleInputChange(event, index)}
                   className={classes.input}
                 />
                 <button
@@ -84,13 +88,31 @@ const ProfileLayer = () => {
                 </button>
               </div>
             ))}
-            <button onClick={handleAddSite} className={classes.button1}>
+            <div className={classes.newSiteRow}>
+              <input
+                type="text"
+                name="name"
+                value={newSite.name}
+                onChange={handleNewSiteChange}
+                placeholder="사이트 이름"
+                className={classes.input}
+              />
+              <input
+                type="text"
+                name="url"
+                value={newSite.url}
+                onChange={handleNewSiteChange}
+                placeholder="사이트 URL"
+                className={classes.input}
+              />
+            </div>
+            <button onClick={handleAddSite} className={classes.addButton}>
               다른사이트 추가하기
             </button>
           </div>
         </div>
-        <div className={classes.AdminBlock3}>
-          <button className={classes.button}>변경사항 저장</button>
+        <div className={classes.adminBlock3}>
+          <button className={classes.saveButton}>변경사항 저장</button>
         </div>
       </div>
     </div>
