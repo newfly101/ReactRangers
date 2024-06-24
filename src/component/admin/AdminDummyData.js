c778c995d4c70f956d8b8d8dc2edb244e725cd2d
Jae-Hong-Kim <newfly101@naver.com>
2024-06-24 10:52:46 +0900

    [admin] dummyData 추가

---
+++ src/component/admin/AdminDummyData.js
@@ -0,0 +1,34 @@
+export const adminBlogDummyData = {
+    "status": 200,
+    "desc": "OK",
+    "data": [
+    {
+        "id": 6553882,
+        "name": "developer-dawn-game",
+        "secondaryDomain": null,
+        "title": "새벽의 이것저것",
+        "defaultBlog": true,
+        "defaultUrl": "https://developer-dawn-game.tistory.com",
+        "adminUrl": "https://developer-dawn-game.tistory.com/manage",
+        "memberCount": 1,
+        "blogImageUrl": "https://tistory1.daumcdn.net/tistory/6553882/attach/d0921d6e591d4aaab257c055924e345a",
+        "useCustomDomain": false,
+        "role": "소유자",
+        "myMemberCount": 0
+    },
+    {
+        "id": 6841237,
+        "name": "df-nexon",
+        "secondaryDomain": null,
+        "title": "PC게임 모음집",
+        "defaultBlog": false,
+        "defaultUrl": "https://df-nexon.tistory.com",
+        "adminUrl": "https://df-nexon.tistory.com/manage",
+        "memberCount": 1,
+        "blogImageUrl": "https://t1.daumcdn.net/tistory_admin/static/manage/images/r3/default_S.png",
+        "useCustomDomain": false,
+        "role": "소유자",
+        "myMemberCount": 0
+    }
+    ]
+}
