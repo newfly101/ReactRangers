af8dd916bfcc5d12ea7a269b100124acae25f318
aa1as <sm13929881@gmail.com>
2024-07-02 22:29:04 +0900

    불필요 Fragment 제거

--- web/src/views/admin/profile/ProfileLayerFilter.js
+++ web/src/views/admin/profile/ProfileLayerFilter.js
@@ -6,15 +6,13 @@ const ProfileLayerFilter = (props) => {
   };
 
   return (
-    <>
-      <div>
-        <label>댓글과 블로그 사이드바에 프로필 레이어를</label>
-        <select value={props.selected} onChange={filterChangeHandler}>
-          <option value="공개하지 않습니다.">공개하지 않습니다.</option>
-          <option value="공개합니다.">공개합니다.</option>
-        </select>
-      </div>
-    </>
+    <div>
+      <label>댓글과 블로그 사이드바에 프로필 레이어를</label>
+      <select value={props.selected} onChange={filterChangeHandler}>
+        <option value="공개하지 않습니다.">공개하지 않습니다.</option>
+        <option value="공개합니다.">공개합니다.</option>
+      </select>
+    </div>
   );
 };
 
