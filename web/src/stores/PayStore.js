f81fe00a995937d5792e7d421d2a5ae521c580cc
KRjasoncho <166813835+KRjasoncho@users.noreply.github.com>
2024-07-03 14:13:25 +0900

    [pay페이지] PortOne을 이용한 결제 시스템 구축

--- web/src/stores/PayStore.js
+++ web/src/stores/PayStore.js
@@ -1,15 +1,31 @@
 import {makeAutoObservable} from "mobx";
 import axios from "axios";
+import * as PortOne from "@portone/browser-sdk/v2";
 
-class PayStore{
+class PayStore {
+//state
 
-  constructor() {
-    makeAutoObservable(this)
-  }
+ constructor() {
+   makeAutoObservable(this)
+ }
 
-  openPayPage =()=>{
+ //action
+ async pay(){
+   const response = await PortOne.requestPayment(
+     {
+       storeId: "store-a7369d77-3179-41d6-b442-16981f8f37e7",
+       channelKey: "channel-key-a9d508d2-3972-4dc8-9509-e5072ff4ea61",
+       paymentId: ` payment-${crypto.randomUUID()}`,
+       orderName: "한푼만 줍쇼",
+       totalAmount: 1000,
+       currency: "CURRENCY_KRW",
+       payMethod: "CARD",
+     }
+   );
+   if (response.code != null){
+     return alert(response.message)
+   }
+ }
 
-  }
 }
-
-export default PayStore
+export default PayStore;
--- web/src/views/pay/Pay.js
+++ web/src/views/pay/Pay.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import classes from "../../css/pay/Pay.module.css"
+import {Observer, useLocalObservable} from "mobx-react";
+import PayStore from "../../stores/PayStore";
+
 
 const Pay = () => {
+  const payStore = useLocalObservable(() => new PayStore());
   return (
-    <div className={classes.payWrapper}>
-      <div>
-        kakaoPay로 불쌍한 개발자에게 후원하기
-      </div>
-      <button>한 푼만 주세요</button>
-    </div>
+    <Observer>
+      {() => (
+        <div className={classes.payWrapper}>
+          <div>
+            PortOne으로 불쌍한 개발자에게 후원하기
+          </div>
+          <button onClick={payStore.pay}>한 푼만 주세요</button>
+        </div>
+      )}
+    </Observer>
   );
 };
 
