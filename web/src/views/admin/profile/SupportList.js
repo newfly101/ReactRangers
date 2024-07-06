e6874cc7bd2edaa78eb28c3f3684e04e6be1b101
aa1as <sm13929881@gmail.com>
2024-07-06 18:52:13 +0900

    [SupportList] 검색버튼 클릭시 입력창 렌더링

--- web/src/views/admin/profile/SupportList.js
+++ web/src/views/admin/profile/SupportList.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "../../../css/admin/profile/SupportList.module.css";
 import MyProfile from "../MyProfile";
 
 const SupportList = () => {
+  const [searchText, setSearchText] = useState(false);
+
+  const searchClickHandler = () => {
+    setSearchText(!searchText);
+  };
+
   return (
     <div className={classes.supportList}>
       <div className={classes.supportListLeft}>
@@ -10,9 +16,19 @@ const SupportList = () => {
       </div>
       <div className={classes.supportListRight}>
         <div className={classes.mainTitle}>응원내역</div>
-        <div className={classes.supportBlock1}>검색기능</div>
-
-        <div className={classes.supportBlock2}>댓글목록</div>
+        <div className={classes.searchBox}>
+          <button className={classes.searchIcon} onClick={searchClickHandler}>
+            검색
+            <img src={"/admin/search.svg"} alt="search" />
+          </button>
+          {searchText && (
+            <div className={classes.searchText}>
+              <input type="text" placeholder="응원 내역에서 검색합니다." />
+              <button type="submit">검색</button>
+            </div>
+          )}
+        </div>
+        <div className={classes.searchList}>응원한 댓글이 없습니다</div>
       </div>
     </div>
   );
