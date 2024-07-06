c65fc1cc34af8888a70d4757f4ac60370bfd4199
aa1as <sm13929881@gmail.com>
2024-07-06 16:06:56 +0900

    [External] ui구현

--- web/src/views/admin/profile/External.js
+++ web/src/views/admin/profile/External.js
@@ -9,8 +9,17 @@ const External = () => {
         <MyProfile />
       </div>
       <div className={classes.externalRight}>
-        <div className={classes.externalTitle}>애플리케이션 목록</div>
-        <div className={classes.serviceBox}>등록일</div>
+        <div className={classes.externalTitle}>
+          애플리케이션 목록
+          <span>내 아이디와 연동된 외부 서비스입니다.</span>
+        </div>
+        <div className={classes.serviceBox}>
+          <span>등록일</span>
+          <span>서비스</span>
+          <span>형태</span>
+          <span>권한</span>
+          <span>해제</span>
+        </div>
 
         <div className={classes.serviceList}>표시할 서비스가 없습니다</div>
       </div>
